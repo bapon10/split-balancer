7e0e32cde6891bd2c459caa193604d7fb3334256
Rayan Banerjee <baponr10032003@gmail.com>
2026-05-02 11:51:47 +0530

    huh

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDM8h9QGwRNZlF3JXzbdSjzzTmRLRFQYc4",
+  authDomain: "splitwiser-shreyan.firebaseapp.com",
+  databaseURL: "https://splitwiser-shreyan-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "splitwiser-shreyan",
+  storageBucket: "splitwiser-shreyan.firebasestorage.app",
+  messagingSenderId: "270734554091",
+  appId: "1:270734554091:web:742e2e9302acac57b4dfae",
+  measurementId: "G-H1MME1T04J"
+};
+
+export default firebaseConfig;
